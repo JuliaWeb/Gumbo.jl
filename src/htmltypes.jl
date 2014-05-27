aceb8ed2240820ca723fc0489f6e43fae02cbd90
James Porter <porterjamesj@gmail.com>
2014-05-27 17:49:03 -0500

    give everything a parent pointer
    this is very convenient and makes lots of things (jQuery-style
    selectors, etc.) easier to implement.

--- src/htmltypes.jl
+++ src/htmltypes.jl
@@ -2,16 +2,23 @@ abstract HTMLNode
 
 # TODO immutable?
 type HTMLText <: HTMLNode
+    parent::HTMLNode
     text::String
 end
 
+# convenience method for defining without parent
+HTMLText(text::String) = HTMLText(NullNode(), text)
+
+type NullNode <: HTMLNode end
+
 type HTMLElement{T} <: HTMLNode
     children::Vector{HTMLNode}
+    parent::HTMLNode
     attributes::Dict{String, String}
 end
 
 # convenience method for defining an empty element
-HTMLElement(T::Symbol) = HTMLElement{T}({},Dict{String,String}())
+HTMLElement(T::Symbol) = HTMLElement{T}(HTMLNode[],NullNode(),Dict{String,String}())
 
 type HTMLDocument
     doctype::String
