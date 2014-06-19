12229a7cac2a5088765f74c6e083059598da7d56
Tony Kelman <tony@kelman.net>
2014-06-19 14:28:29 -0700

    add windows binary download

--- deps/build.jl
+++ deps/build.jl
@@ -2,12 +2,15 @@ using BinDeps
 
 @BinDeps.setup
 
-libgumbo = library_dependency("libgumbo")
+libgumbo = library_dependency("libgumbo", aliases = ["libgumbo-1"])
 
 provides(Sources,
          URI("http://jamesporter.me/static/julia/gumbo-1.0.tar.gz"),
          libgumbo,
          unpacked_dir="gumbo-1.0")
+
+provides(Binaries, URI("http://sourceforge.net/projects/juliadeps-win/files/gumbo-$(Sys.MACHINE).7z"),
+         libgumbo, os = :Windows)
 
 provides(BuildProcess,
          Autotools(libtarget="libgumbo.la"),
